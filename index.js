53562d4db6152e7d3acd2569550802cb512a91af
granddivino <granddivino@gmail.com>
2020-10-28 13:39:31 -0600

    Gabriel G. - Mateen: Pokedex Deliverable

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require('express');
 const axios = require('axios'); 
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
+
+const db = require("./models");
+
+app.use(express.static('public'))
 
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
@@ -12,12 +16,13 @@ app.use(ejsLayouts);
 
 // GET / - main index of site
 app.get('/', function(req, res) {
-  const pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  const pokemonUrl = 'http://pokeapi.co/api/v2/pokemon?limit=152';
   // Use request to call the API
   axios.get(pokemonUrl).then( function(apiResponse) {
     const pokemon = apiResponse.data.results;
-    res.render('index', { pokemon: pokemon.slice(0, 151) });
+    res.render('index', { pokemon: pokemon });
   })
+  axios.get
 });
 
 // Imports all routes from the pokemon routes file
